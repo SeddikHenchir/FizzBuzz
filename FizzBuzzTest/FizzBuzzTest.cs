c80a827876e02534c474edd6906d6198002ae4d2
Seddik HENCHIR <seddik.henchir@softeam.fr>
2019-04-01 16:32:47 +0200

    Add project files.

---
+++ FizzBuzzTest/FizzBuzzTest.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class FizzBuzzTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void NumericValue()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public void NonNegativeValue()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public void Multiple_3()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public void Multiple_5()
+        {
+            Assert.Pass();
+        }
+        public void Multiple_5_3()
+        {
+            Assert.Pass();
+        }
+    }
+}
